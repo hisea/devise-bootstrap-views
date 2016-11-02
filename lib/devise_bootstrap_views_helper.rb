65ca2d9177aa9aa589f31fa07140eeeab99b26d3
Dan Kim <itsdanya@gmail.com>
2016-11-02 17:48:44 +0300

    Update alert for error messages

--- lib/devise_bootstrap_views_helper.rb
+++ lib/devise_bootstrap_views_helper.rb
@@ -2,16 +2,20 @@ module DeviseBootstrapViewsHelper
   def bootstrap_devise_error_messages!
     return '' if resource.errors.empty?
 
-    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
-    sentence = I18n.t('errors.messages.not_saved',
+    messages = resource.errors.full_messages.map { |message| content_tag(:li, message) }.join
+    sentence = I18n.t(
+      'errors.messages.not_saved',
       count: resource.errors.count,
-      resource: resource.class.model_name.human.downcase)
+      resource: resource.class.model_name.human.downcase
+    )
 
     html = <<-HTML
-    <div class="alert alert-danger alert-block devise-bs">
-      <button type="button" class="close" data-dismiss="alert">&times;</button>
-      <h5>#{sentence}</h5>
-      <ul>#{messages}</ul>
+    <div class="alert alert-danger">
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+      <h4 class="alert-heading">#{sentence}</h4>
+      <ul class="mb-0">#{messages}</ul>
     </div>
     HTML
 
